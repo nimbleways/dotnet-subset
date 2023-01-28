62f959aaa6abd41126938d6391afb264d52da9c5
Othmane Kinane <76955412+othmane-kinane-nw@users.noreply.github.com>
2023-01-28 23:41:18 +0100

    print tool and runtime versions

---
+++ src/dotnet-subset/Helpers.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace Nimbleways.Tools.Subset;
+internal static class Helpers
+{
+    public static void PrintContext()
+    {
+        string toolName = Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]);
+        Version version = Assembly.GetExecutingAssembly().GetName().Version;
+        Console.WriteLine($"{toolName} {version.ToString(3)} (.NET Runtime {Environment.Version})");
+    }
+}
