54d37655584d8dc59d48effdaf8390a3b3d6b6c6
Othmane KINANE <76955412+othmane-kinane-nw@users.noreply.github.com>
2023-05-08 20:25:19 +0100

    Add the first functional test (#10)

---
+++ test/dotnet-subset.FunctionalTests/GuardExtensions.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Nimbleways.Tools.Subset;
+
+internal static class GuardExtensions
+{
+    // ! Value is provided at compile-time
+    [return: NotNull]
+    public static T AsNotNull<T>(this T? obj, [CallerArgumentExpression("obj")] string expression = null!)
+        where T : class?
+    {
+        return obj is null ? throw new UnexpectedNullExpressionException(expression) : obj;
+    }
+}
+
+[SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "<Pending>")]
+public class UnexpectedNullExpressionException : Exception
+{
+    public UnexpectedNullExpressionException(string expression)
+        : base($"Expression '{expression}' is null")
+    {
+    }
+}
